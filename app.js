ddcbdce0ac459b62d41c77c9ea74180335c76330
Eugene Posokhin <homomodern@gmail.com>
2025-01-12 20:49:01 +0600

    WIP

--- app.js
+++ app.js
@@ -6,133 +6,143 @@ import { Readable } from 'node:stream'
 import { pipeline } from 'node:stream/promises'
 
 const config = {
-  options: {
-    help: {
-      type: 'boolean',
-      short: 'h'
+    options: {
+        help: {
+            type: 'boolean',
+            short: 'h'
+        },
+        path: {
+            type: 'string',
+            short: 'p',
+            default: `${process.env.HOME}/Music`
+        },
+        track: {
+            type: 'string',
+            short: 't'
+        },
+        simultaneous: {
+            type: 'string',
+            short: 's',
+            default: '5'
+        }
     },
-    path: {
-      type: 'string',
-      short: 'p',
-      default: `${process.env.HOME}/Music`
-    },
-    track: {
-      type: 'string',
-      short: 't'
-    },
-    simultaneous: {
-      type: 'string',
-      short: 's',
-      default: '5'
-    }
-  },
-  allowPositionals: true
+    allowPositionals: true
 }
 
 const cleanUpSymbols = inputString => inputString.replace(/[:/"*<>|?]/g, '')
 
-function getLinksAndTags (html, domain) {
-  const $ = cheerio.load(html)
+function getLinksAndTags(html, domain) {
+    const $ = cheerio.load(html)
 
-  const [album, artist = 'VA'] = $('h1')
-    .text()
-    .trim()
-    .split(' - ', 2)
-    .reverse()
-  const tracksData = []
-  const $tracks = $('.playlist__item')
-  const coverURL = $('.album-img').attr('data-src')
+    const [album, artist = 'VA'] = $('h1')
+        .text()
+        .trim()
+        .split(' - ', 2)
+        .reverse()
+    const tracksData = []
+    const $tracks = $('.playlist__item')
+    const coverURL = $('.album-img').attr('data-src')
 
-  $tracks.each((index, element) => {
-    let trackNo = $(element)
-      .find('.playlist__position')
-      .text()
-      .trim()
-    if (trackNo.length < 2) trackNo = '0' + trackNo
+    $tracks.each((index, element) => {
+        let trackNo = $(element)
+            .find('.playlist__position')
+            .text()
+            .trim()
+        if (trackNo.length < 2) trackNo = '0' + trackNo
 
-    tracksData.push({
-      url: `https://${domain}${$(element)
+        tracksData.push({
+            url: `https://${domain}${$(element)
                 .find('.playlist__control.play')
                 .attr('data-url')}`,
-      trackNo,
-      title: $(element)
-        .find('.playlist__details a.strong')
-        .text()
-        .trim(),
-      artist,
-      album
+            trackNo,
+            title: $(element)
+                .find('.playlist__details a.strong')
+                .text()
+                .trim(),
+            artist,
+            album
+        })
     })
-  })
 
-  return { tracksData, coverURL }
+    return { tracksData, coverURL }
 }
 
-async function prepareAlbumDir (path) {
-  const newDir = await mkdir(path, { recursive: true })
-  if (!newDir) return
-  console.log(`Created ${newDir}`)
+async function prepareAlbumDir(path) {
+    const newDir = await mkdir(path, { recursive: true })
+    if (!newDir) return
+    console.log(`Created ${newDir}`)
 }
 
-async function downloadFile (url, filename) {
-  const res = await fetch(url)
+async function downloadFile(url, filename) {
+    let filehandle
+    try {
+        filehandle = await open(filename, 'wx')
+        const writer = filehandle.createWriteStream()
 
-  if (res.status !== 200) {
-    console.log(`The file ${filename} cannot be downloaded`)
-    return
-  }
-  console.log(`Start downloading: ${filename}`)
+        const res = await fetch(url)
+        if (!res.ok) {
+            console.warn(`Server responds with status ${res.status}. The file at ${url} cannot be downloaded`)
+            return
+        }
+        console.info(`Start downloading: ${url}`)
 
-  const filehandle = await open(filename, 'wx')
-
-  const writer = filehandle.createWriteStream()
-
-  await pipeline(res.body, Readable.fromWeb, writer)
-  await filehandle.close()
-  console.log(`Finished: ${filename}`)
+        await pipeline(res.body, Readable.fromWeb, writer)
+        console.info(`Finished: ${filename}`)
+    } catch (error) {
+        if (error.cause?.code === 'ENOTFOUND') {
+            console.warn(`${url} not found`)
+        } else if (error.code === 'EEXIST') {
+            console.warn(`${filename} exist. Skipping`)
+        } else {
+            throw error
+        }
+    } finally {
+        await filehandle?.close()
+    }
 }
 
-async function downloadTracks (tracks, path, simNum) {
-  if (tracks.length === 0) return
-  const toDownload = tracks.slice(0, simNum)
+async function downloadTracks(tracks, path, simNum) {
+    if (tracks.length === 0) return
+    const toDownload = tracks.slice(0, simNum)
 
-  const pending = toDownload.map(async track => {
-    const filename = `${path}/${track.trackNo} - ${track.title}.mp3`
-    await downloadFile(track.url, filename)
-  })
+    const pending = toDownload.map(async track => {
+        const filename = `${path}/${track.trackNo} - ${track.title}.mp3`
+        await downloadFile(track.url, filename)
+    })
 
-  await Promise.allSettled(pending)
+    await Promise.allSettled(pending)
 
-  return downloadTracks(tracks.slice(simNum), path, simNum)
+    return downloadTracks(tracks.slice(simNum), path, simNum)
 }
 
 try {
-  const parsedArgs = parseArgs(config)
-  const {
-    path,
-    simultaneous
-  } = parsedArgs.values
+    const parsedArgs = parseArgs(config)
+    const {
+        path,
+        simultaneous
+    } = parsedArgs.values
 
-  const albumURL = parsedArgs.positionals[0]
+    const albumURL = parsedArgs.positionals[0]
 
-  const domain = new URL(albumURL).hostname
+    const domain = new URL(albumURL).hostname
 
-  const res = await fetch(albumURL)
-  const body = await res.text()
-  const { tracksData, coverURL } = getLinksAndTags(body, domain)
+    const res = await fetch(albumURL)
+    const body = await res.text()
+    const { tracksData, coverURL } = getLinksAndTags(body, domain)
 
-  const tracksDateCleaned = tracksData.map(track => {
-    return {
-      ...track,
-      title: cleanUpSymbols(track.title),
-      artist: cleanUpSymbols(track.artist),
-      album: cleanUpSymbols(track.album)
-    }
-  })
+    const tracksDateCleaned = tracksData.map(track => {
+        return {
+            ...track,
+            title: cleanUpSymbols(track.title),
+            artist: cleanUpSymbols(track.artist),
+            album: cleanUpSymbols(track.album)
+        }
+    })
 
-  const albumPath = `${path}/${tracksDateCleaned[0].artist}/${tracksDateCleaned[0].album}`
-  console.log(albumPath)
-  await prepareAlbumDir(albumPath)
-  await downloadTracks(tracksDateCleaned, albumPath, +simultaneous)
+    const albumPath = `${path}/${tracksDateCleaned[0].artist}/${tracksDateCleaned[0].album}`
+    console.log(albumPath)
+    await prepareAlbumDir(albumPath)
+    await downloadTracks(tracksDateCleaned, albumPath, +simultaneous)
 } catch (error) {
-  console.error(error.message)
+    console.error(error.message)
 }
